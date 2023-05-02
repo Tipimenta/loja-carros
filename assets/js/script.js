23cf1b62db8cd794d89ea590edf80265706305bb
Tipimenta <tiagopimenta.ata@gmail.com>
2023-05-02 16:06:06 -0300

    atualizacao do projeto borssatto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,51 @@
-const button = document.querySelector(".card-content");
-const button1 = document.querySelector(".card-content1");
-const modal = document.getElementById("mymodal");
-const modal1 = document.getElementById("mymodal1");
-const close = document.querySelector(".close");
-const close1 = document.querySelector(".close1");
 
-button.addEventListener('click', () => {
-    modal.classList.add('modal-active');
-})
+// Seleciona todos os botões "Abrir modal"
+const openModalButtons = document.querySelectorAll('.card-content');
 
-close.addEventListener('click', () => {
-    modal.classList.remove('modal-active');
-})
+// Seleciona todos os botões "Fechar"
+const closeModalButtons = document.querySelectorAll('.close');
 
-button1.addEventListener('click', () => {
-    modal1.classList.add('modal-active');
-})
+// Seleciona todos os modais
+const modals = document.querySelectorAll('.modal');
 
-close1.addEventListener('click', () => {
-    modal1.classList.remove('modal-active');
-})
+// Adiciona um event listener para cada botão "Abrir modal"
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Obtém o ID do modal a ser aberto a partir do atributo "data-modal-id" do botão
+    const modalId = button.dataset.modalId;
+
+    // Seleciona o modal correto com base no ID
+    const modal = document.getElementById(modalId);
+
+    // Adiciona a classe "open" ao modal para exibi-lo
+    modal.classList.add('open');
+  });
+});
+
+// Adiciona um event listener para cada botão "Fechar"
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Obtém o modal pai do botão
+    const modal = button.closest('.modal');
+
+    // Remove a classe "open" do modal para escondê-lo
+    modal.classList.remove('open');
+  });
+});
+
+// Adiciona um event listener para cada modal
+modals.forEach(modal => {
+  modal.addEventListener('click', event => {
+    // Verifica se o clique ocorreu fora do conteúdo do modal
+    if (event.target === modal) {
+      // Remove a classe "open" do modal para escondê-lo
+      modal.classList.remove('open');
+    }
+  });
+});
+
+function thumbChange(num){
+  var thumb ='../assets/Image/carro' + num + '.jpeg';
+  document.getElementById("mainPic").src = thumb;
+  document.getElementById("mainPic1").src = thumb;
+}
